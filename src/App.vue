7475af229ff54222865b196ad8ea8c68f063d224
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-03 23:58:25 +0900

    feat: App.vue에 부동산 정보 추가

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <h1 :style="blueText">Vuedongsan</h1>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      price1: 60,
+      price2: 70,
+      blueText: "color: blue",
+      products: ["역삼동 원룸", "천호동 원룸", "마포구 원룸"],
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
