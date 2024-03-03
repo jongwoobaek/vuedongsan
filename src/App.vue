7bd5c0ef5d0a0dcd378bac8eafa157d842f35aa9
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-04 00:29:05 +0900

    refactor: App.vue 상품 목록을 반복문으로 수정

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,10 @@
     <a v-for="(menu, index) in menuList" :key="index">{{ menu }}</a>
   </div>
 
-  <div>
-    <h1 :style="blueText">Vuedongsan</h1>
-    <h4>{{ products[0] }}</h4>
-    <p>{{ price1 }} 만원</p>
-  </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>{{ price2 }} 만원</p>
-  </div>
-  <div>
-    <h4>{{ products[2] }}</h4>
-    <p>{{ price2 }} 만원</p>
+  <h1 :style="blueText">Vuedongsan</h1>
+  <div v-for="(product, index) in products" :key="index">
+    <h4>{{ product.name }}</h4>
+    <p>{{ product.price }} 만원</p>
   </div>
 </template>
 
@@ -23,10 +15,21 @@ export default {
   name: "App",
   data() {
     return {
-      price1: 60,
-      price2: 70,
       blueText: "color: blue",
-      products: ["역삼동 원룸", "천호동 원룸", "마포구 원룸"],
+      products: [
+        {
+          name: "역삼동 원룸",
+          price: 60,
+        },
+        {
+          name: "천호동 원룸",
+          price: 70,
+        },
+        {
+          name: "마포구 원룸",
+          price: 80,
+        },
+      ],
       menuList: ["Home", "Shop", "About"],
     };
   },
