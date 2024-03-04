f67ceccba9088471a251ce63d02464fb7f22f971
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-05 00:39:34 +0900

    feat: components 폴더에 DiscountBanner 구현

---
+++ src/components/DiscountBanner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="discount-banner">
+    <h4>지금 결제하면 20% 할인</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DiscountBanner",
+};
+</script>
+
+<style scoped>
+.discount-banner {
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #eee;
+}
+</style>
