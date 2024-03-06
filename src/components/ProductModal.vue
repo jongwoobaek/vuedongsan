cf7f90e723da359bbdd3852e384000809a0b304a
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-06 23:25:30 +0900

    feat: ProductModal watcher에 month 함수 구현

--- src/components/ProductModal.vue
+++ src/components/ProductModal.vue
@@ -22,6 +22,14 @@ export default {
       month: 1,
     };
   },
+  watch: {
+    month(userInput, prevData) {
+      if (userInput < 1 || userInput > 12) {
+        alert("1 ~ 12 사이의 숫자만 입력할 수 있습니다!");
+        this.month = prevData;
+      }
+    },
+  },
   props: {
     isOpen: Boolean,
     product: Object,
